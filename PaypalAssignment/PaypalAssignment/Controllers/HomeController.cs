92fae735a299c9e1a6fc779353f58f5a65c79276
Mario Hugo C Flores <Mariocf_93@hotmail.com>
2016-02-25 09:34:47 -0800

    need to host for paypal action to get call

--- PaypalAssignment/PaypalAssignment/Controllers/HomeController.cs
+++ PaypalAssignment/PaypalAssignment/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using PaypalAssignment.BusinessLogic;
 
 namespace PaypalAssignment.Controllers
 {
@@ -11,8 +12,30 @@ namespace PaypalAssignment.Controllers
         // GET: Home
         public ActionResult Index()
         {
+            DeveloPaypalEntities context = new DeveloPaypalEntities();
+            return View(context.IPNs);
+        }
+        public ActionResult PayPal()
+        {
+            Paypal_IPN paypalResponse = new Paypal_IPN("test");
+
+            if (paypalResponse.TXN_ID != null)
+            {
+                DeveloPaypalEntities context = new DeveloPaypalEntities();
+                IPN ipn = new IPN();
+                ipn.transactionID = paypalResponse.TXN_ID;
+                decimal amount = Convert.ToDecimal(paypalResponse.PaymentGross);
+                ipn.amount = amount;
+                ipn.buyerEmail = paypalResponse.PayerEmail;
+                ipn.txTime = DateTime.Now;
+                ipn.custom = paypalResponse.Custom;
+                ipn.paymentStatus = paypalResponse.PaymentStatus;
+                context.IPNs.Add(ipn);
+                context.SaveChanges();
+            }
             return View();
         }
+
         public ActionResult Cancel()
         {
             return View();
